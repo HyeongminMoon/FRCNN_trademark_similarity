1cd01715293da20b5ebe4d74de6c3e1e43d21dab
HyeongminMoon <mohomin123@gmail.com>
2020-08-13 13:46:36 +0900

    seperate model

--- maskrcnn_pennfudan.py
+++ maskrcnn_pennfudan.py
@@ -67,48 +67,95 @@ class ViennaDataset(torch.utils.data.Dataset):
         
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
 
-        code = int(self.imgs[idx][0])
-        human = [1,4,5]
-        animal = [7,10,11,12,15,37]
-        circle = [8,19,24,25,31,35,49]
-        bowl = [14,20,30,48]
-        triangle = [3,6,9,16,18,47,52]
-        rect = [13,17,29,43,55]
-        tree = [46,51]
-        car = [21,26,53]
-        tool = [23,33,45]
-        hat = [2,34,36,50]
-        cross = [27,28,32,54]
-        part = [22,41]
-        landscape = [38,39,40,42,44]
-        if code is in human:
+        code = int(self.imgs[idx][0:2])
+        human = [1,4,5,7,10,11,12,15,37]
+        sun = [8]
+        planet = [19]
+        heart = [35]
+        ceramic = [14]
+        bottle = [48]
+        bowl = [30]
+        star = [3]
+        jewel = [6]
+        moon = [9]
+        triangle = [47]
+        arrow = [52]
+        rect = [43]
+        shield = [55]
+        building = [13]
+        tree = [46]
+        flower = [51]
+        car = [53]
+        airplane = [26]
+        crown = [2]
+        hat = [34]
+        cross = [27, 32]
+        landscape = [38]
+        mountain = [40]
+        plantac = [42]
+        leaf = [18]
+        fruit = [49]
+        tool = [33, 45]
+
+        labels = torch.full((num_objs,), 0, dtype=torch.int64)
+        if code in human:
             labels = torch.full((num_objs,), 1, dtype=torch.int64)
-        elif code is in animal:
+        elif code in sun:
             labels = torch.full((num_objs,), 2, dtype=torch.int64)
-        elif code is in circle:
+        elif code in planet:
             labels = torch.full((num_objs,), 3, dtype=torch.int64)
-        elif code is in bowl:
+        elif code in heart:
             labels = torch.full((num_objs,), 4, dtype=torch.int64)
-        elif code is in triangle:
+        elif code in ceramic:
             labels = torch.full((num_objs,), 5, dtype=torch.int64)
-        elif code is in rect:
+        elif code in bottle:
             labels = torch.full((num_objs,), 6, dtype=torch.int64)
-        elif code is in tree:
+        elif code in bowl:
             labels = torch.full((num_objs,), 7, dtype=torch.int64)
-        elif code is in car:
+        elif code in star:
             labels = torch.full((num_objs,), 8, dtype=torch.int64)
-        elif code is in tool:
+        elif code in jewel:
             labels = torch.full((num_objs,), 9, dtype=torch.int64)
-        elif code is in hat:
+        elif code in moon:
             labels = torch.full((num_objs,), 10, dtype=torch.int64)
-        elif code is in cross:
+        elif code in triangle:
             labels = torch.full((num_objs,), 11, dtype=torch.int64)
-        elif code is in part:
+        elif code in arrow:
             labels = torch.full((num_objs,), 12, dtype=torch.int64)
-        elif code is in landscape:
+        elif code in rect:
             labels = torch.full((num_objs,), 13, dtype=torch.int64)
+        elif code in shield:
+            labels = torch.full((num_objs,), 14, dtype=torch.int64)
+        elif code in building:
+            labels = torch.full((num_objs,), 15, dtype=torch.int64)
+        elif code in tree:
+            labels = torch.full((num_objs,), 16, dtype=torch.int64)
+        elif code in flower:
+            labels = torch.full((num_objs,), 17, dtype=torch.int64)
+        elif code in car:
+            labels = torch.full((num_objs,), 18, dtype=torch.int64)
+        elif code in airplane:
+            labels = torch.full((num_objs,), 19, dtype=torch.int64)
+        elif code in crown:
+            labels = torch.full((num_objs,), 20, dtype=torch.int64)
+        elif code in hat:
+            labels = torch.full((num_objs,), 21, dtype=torch.int64)
+        elif code in cross:
+            labels = torch.full((num_objs,), 22, dtype=torch.int64)
+        elif code in landscape:
+            labels = torch.full((num_objs,), 23, dtype=torch.int64)
+        elif code in mountain:
+            labels = torch.full((num_objs,), 24, dtype=torch.int64)
+        elif code in plantac:
+            labels = torch.full((num_objs,), 25, dtype=torch.int64)
+        elif code in leaf:
+            labels = torch.full((num_objs,), 26, dtype=torch.int64)
+        elif code in fruit:
+            labels = torch.full((num_objs,), 27, dtype=torch.int64)
+        elif code in tool:
+            labels = torch.full((num_objs,), 28, dtype=torch.int64)
         else:
-            print("no code error")
+            print("no code error, code: ", code)
         
         #labels = torch.full((num_objs,), int(self.imgs[idx][0:2]), dtype=torch.int64)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
@@ -285,7 +332,7 @@ def temp():
     dataset_test = torch.utils.data.Subset(dataset_test, indices[-t:])
     # can error: try num_workers=0
     data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
+        dataset, batch_size=1, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
     )
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
@@ -308,8 +355,12 @@ def temp():
               'circle','cross','food','hat','heart','helmat','insect','landscape','map','mountain',
               'part','plantac','rect','river','tool','tree','triangle','bottle','fruit','phone',
               'flower','arrow','car','ribbon','shield']
-    num_classes = 14
-    labels = ['background', 'human', 'animal', 'circle', 'bowl', 'triangle', 'rect', 'tree', 'car', 'tool', 'hat', 'cross', 'part', 'landsacpe']
+    num_classes = 29
+    labels = ['background', 'human', 'sun', 'planet', 'heart', 'ceramic', 'bottle', 'bowl', 'star', 'jewel',
+              'moon','triangle','arrow','rect','shield','building','tree','flower','car','airplane',
+              'crown','hat','cross','landscape','mountain','plantac','leaf','fruit','tool']
+    #num_classes = 14
+    #labels = ['background', 'human', 'animal', 'circle', 'bowl', 'triangle', 'rect', 'tree', 'car', 'tool', 'hat', 'cross', 'part', 'landsacpe']
     #human = ['man', 'child', 'woman']
     #animal = ['animal','aqua','bird','amphibia','dinosaur','insect']
     #circle = ['sun', 'circle','heart','planet','wheel','watch','fruit']
@@ -351,7 +402,7 @@ def temp():
         optimizer = torch.optim.SGD(params, lr=0.004, momentum=0.9, weight_decay=0.0005)
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.7)
 
-        num_epochs = 15
+        num_epochs = 20
 
         for epoch in range(num_epochs):
             train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=1)
@@ -367,7 +418,6 @@ def temp():
 
     print(len(dataset_test))
     for i in range(len(dataset_test)):
-        print(i)
         if i == 302 or i == 601 or i == 723 : continue
         #torch.cuda.empty_cache()
         img, _ = dataset_test[i]
@@ -389,9 +439,9 @@ def temp():
         p = []
         num = 0
         for j in range(num_boxes):
-            #if prediction[0]['scores'][j].cpu().numpy() < 0.3 :
-            #    continue
-            if num >= 7: break
+            if prediction[0]['scores'][j].cpu().numpy() < 0.2 :
+                continue
+            if num >= 3: break
             str_ = labels[prediction[0]['labels'][j].cpu().numpy()]
             if str_ in p: continue
             c0 = prediction[0]['boxes'][j].cpu().numpy()
