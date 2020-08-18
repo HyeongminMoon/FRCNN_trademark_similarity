c9001c444da2583b62a5540ed2e21ad2960eedde
HyeongminMoon <mohomin123@gmail.com>
2020-08-18 17:57:55 +0900

    beta model

--- maskrcnn_pennfudan.py
+++ maskrcnn_pennfudan.py
@@ -30,21 +30,27 @@ __len__은 이미지의 길이를 반환.
 """
 class ViennaDataset(torch.utils.data.Dataset):
 
-    def __init__(self, root, transforms=None):
+    def __init__(self, root, transforms=None, test_mode = False):
         self.root = root
         self.transforms = transforms
+        self.test_mode = test_mode
         # Load images, sorting them
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "00/image"))))
-        #self.masks = list(sorted(os.listdir(os.path.join(root, "00/mask"))))
-
+        if test_mode:
+            self.imgs = list(sorted(os.listdir(root)))
+        else:
+            self.imgs = list(sorted(os.listdir(os.path.join(root, "00/image"))))
 
     def __getitem__(self, idx):
+
+        if self.test_mode:
+            img_path = os.path.join(self.root, self.imgs[idx])
+            img = Image.open(img_path).convert("RGB")
+            if self.transforms is not None:
+                img = self.transforms(img)
+            return img, {}
+            
         img_path = os.path.join(self.root, "00/image", self.imgs[idx])
         mask_path = os.path.join(self.root, "00/mask", self.imgs[idx].replace(".jpg","_mask.png"))
-        
-        #img_path = os.path.join(self.root, "crown_woman/images/2big_img_105.jpg")
-        #mask_path = os.path.join(self.root, "crown_woman/mask/21big_img_105_mask.png")
-
         
         img = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path).convert("L")
@@ -52,7 +58,6 @@ class ViennaDataset(torch.utils.data.Dataset):
 
         np.set_printoptions(threshold=sys.maxsize)
         obj_ids = np.unique(mask)
-        #print(obj_ids)
         obj_ids = obj_ids[1:]
         masks = mask == obj_ids[:, None, None]
         num_objs = len(obj_ids)
@@ -68,6 +73,28 @@ class ViennaDataset(torch.utils.data.Dataset):
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
 
         code = int(self.imgs[idx][0:2])
+        labels = torch.full((num_objs,), 0, dtype=torch.int64)
+        if code == 1:
+            labels = torch.full((num_objs,), 1, dtype=torch.int64)
+        elif code == 4:
+            labels = torch.full((num_objs,), 1, dtype=torch.int64)
+        elif code == 5:
+            labels = torch.full((num_objs,), 1, dtype=torch.int64)
+        elif code == 7:
+            labels = torch.full((num_objs,), 2, dtype=torch.int64)
+        elif code == 10:
+            labels = torch.full((num_objs,), 2, dtype=torch.int64)
+        elif code == 11:
+            labels = torch.full((num_objs,), 2, dtype=torch.int64)
+        elif code == 12:
+            labels = torch.full((num_objs,), 3, dtype=torch.int64)
+        elif code == 15:
+            labels = torch.full((num_objs,), 2, dtype=torch.int64)
+        elif code == 37:
+            labels = torch.full((num_objs,), 4, dtype=torch.int64)
+        else:
+            print("code non exist")
+        """
         human = [1,4,5,7,10,11,12,15,37]
         sun = [8]
         planet = [19]
@@ -156,7 +183,7 @@ class ViennaDataset(torch.utils.data.Dataset):
             labels = torch.full((num_objs,), 28, dtype=torch.int64)
         else:
             print("no code error, code: ", code)
-        
+        """
         #labels = torch.full((num_objs,), int(self.imgs[idx][0:2]), dtype=torch.int64)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
@@ -171,8 +198,6 @@ class ViennaDataset(torch.utils.data.Dataset):
         target["image_id"] = image_id
         target["area"] = area
         target["iscrowd"] = iscrowd #this image has single object?
-        #print(img)
-        #print(target)
         if self.transforms is not None:
             img = self.transforms(img)
 
@@ -205,7 +230,6 @@ class PennFudanDataset(torch.utils.data.Dataset):
         obj_ids = obj_ids[1:]
 
         masks = mask == obj_ids[:, None, None]
-        #print(masks)
         num_objs = len(obj_ids)
         boxes = []
         for i in range(num_objs):
@@ -231,8 +255,6 @@ class PennFudanDataset(torch.utils.data.Dataset):
         target["image_id"] = image_id
         target["area"] = area
         target["iscrowd"] = iscrowd
-        #print(img)
-        #print(target)
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
@@ -264,22 +286,11 @@ def save_mask_from_json(json_path,img_path,save_path):
             rr, cc = skimage.draw.polygon(all_points_y,all_points_x)
             mask[rr,cc] = i+1
         # save mask
-        
-        #img2 = Image.fromarray(mask)
-        #if img2.mode != 'RGB':
-        #    img2 = img2.convert('RGB')
-        #fp2 = open(save_path+img_name.replace('.jpg','_mask.jpg'), 'w')
-        #img2.save(fp2, "JPEG")
-        #fp2.close()
 
         png_img = png.from_array(mask, mode="L;16")
         png_img.save(save_path+img_name.replace('.jpg', '_mask.png'))
-
-    #img_size = ???
-    #point x,y = ???
     img_size = 100
     mask = np.zeros(img_size)
-
 
     return mask
 
@@ -313,6 +324,98 @@ def change_name(path,num):
     for i in range(len(imgs)):
         os.rename(path + imgs[i],path + str(num)+imgs[i])
 
+
+def extract_code():
+
+    root = '../../images'
+    dataset_test = ViennaDataset(root, get_transform(train=False), test_mode = True)
+    #dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
+    torch.manual_seed(1)
+    # can error: try num_workers=0
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
+    )
+
+    # for GPU. if you use CPU, remove below lines
+    torch.cuda.get_device_name(1)
+    use_cuda = torch.cuda.is_available()
+    # use_cuda = False
+    if use_cuda:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    #define classes
+    num_classes = 56
+    labels = ['background','woman','crown','star','man','child','jewel','animal','sun','moon','amphibia',
+              'aqua','bird','building','ceramic','dinosaur','flag','furniture','leaf','planet','plate',
+              'ship','shoes','stationery','watch','wheel','airplane','anchor','atom','book','bowl',
+              'circle','cross','food','hat','heart','helmat','insect','landscape','map','mountain',
+              'part','plantac','rect','river','tool','tree','triangle','bottle','fruit','phone',
+              'flower','arrow','car','ribbon','shield']
+    num_classes = 29
+    labels = ['background', 'character', 'sun', 'planet', 'heart', 'ceramic', 'bottle', 'bowl', 'star', 'jewel',
+              'moon','triangle','arrow','rect','shield','building','tree','flower','car','airplane',
+              'crown','hat','cross','landscape','mountain','plantac','leaf','fruit','tool']
+    
+    #num_classes = 10
+    #labels = ['background', 'woman','man','child','animal','amphibia','aqua','bird','dinosaur','insect']
+    #num_classes = 5
+    #labels = ['background', 'human', 'animal', 'bird', 'insect']
+    
+    #num_classes = 14
+    #labels = ['background', 'human', 'animal', 'circle', 'bowl', 'triangle', 'rect', 'tree', 'car', 'tool', 'hat', 'cross', 'part', 'landsacpe']
+
+    # load model. if none, train.
+    if (os.path.isfile('image_model.pt')):
+        model = torch.load('image_model.pt', map_location=device)
+    else:
+        print("model doesn't exist")
+    # test
+    shutil.rmtree('results/')
+    os.mkdir('results')
+
+    print(len(dataset_test))
+    for i in range(min(500,len(dataset_test))):
+        img, _ = dataset_test[i]
+        model.eval()
+        with torch.no_grad():
+            prediction = model([img.to(device)])
+        im1 = Image.fromarray(img.mul(200).permute(1, 2, 0).byte().numpy())
+        num_boxes = len(prediction[0]['boxes'])
+        colors = pkl.load(open("pallete", "rb"))
+        im1_ = ImageDraw.Draw(im1)
+        #top 3
+        predicted_labels = ''
+        p = []
+        num = 0
+        for j in range(num_boxes):
+            if prediction[0]['scores'][j].cpu().numpy() < 0.2 :
+                continue
+            if num >= 3: break
+            str_ = labels[prediction[0]['labels'][j].cpu().numpy()]
+            if str_ in p: continue
+            c0 = prediction[0]['boxes'][j].cpu().numpy()
+            c1 = tuple(c0[:2])
+            c2 = tuple(c0[2:])
+            im2 = Image.fromarray(prediction[0]['masks'][j, 0].mul(100).byte().cpu().numpy())
+            color = random.choice(colors)
+            layer = Image.new('RGB', im2.size, color)
+            # bounding box
+            im1_.rectangle((c1, c2), outline=color)
+            # label
+            predicted_labels = predicted_labels + '_' + str_
+            score_ = prediction[0]['scores'][j].cpu().numpy()
+            score = np.round(score_, 4)
+            im1_.text(c1, str_ + " score:" + str(score), fill=(255, 255, 255, 255))
+            # mask layer
+            im1.paste(layer, (0, 0), im2)
+            p.append(str_)
+            num += 1
+
+        fp1 = open('results/'+str(i)+'_detection'+predicted_labels+'.jpg', 'w')
+        im1.save(fp1, "JPEG")
+        fp1.close()
 
 def temp():
     # simple check for dataset
@@ -356,9 +459,13 @@ def temp():
               'part','plantac','rect','river','tool','tree','triangle','bottle','fruit','phone',
               'flower','arrow','car','ribbon','shield']
     num_classes = 29
-    labels = ['background', 'human', 'sun', 'planet', 'heart', 'ceramic', 'bottle', 'bowl', 'star', 'jewel',
+    labels = ['background', 'character', 'sun', 'planet', 'heart', 'ceramic', 'bottle', 'bowl', 'star', 'jewel',
               'moon','triangle','arrow','rect','shield','building','tree','flower','car','airplane',
               'crown','hat','cross','landscape','mountain','plantac','leaf','fruit','tool']
+    num_classes = 10
+    labels = ['background', 'woman','man','child','animal','amphibia','aqua','bird','dinosaur','insect']
+    num_classes = 5
+    labels = ['background', 'human', 'animal', 'bird', 'insect']
     #num_classes = 14
     #labels = ['background', 'human', 'animal', 'circle', 'bowl', 'triangle', 'rect', 'tree', 'car', 'tool', 'hat', 'cross', 'part', 'landsacpe']
     #human = ['man', 'child', 'woman']
@@ -379,7 +486,7 @@ def temp():
     # load model. if none, train.
     if (os.path.isfile(root+'_model.pt')):
         model = torch.load(root+'_model.pt', map_location=device)
-        #evaluate(model, data_loader_test, device=device)
+        evaluate(model, data_loader_test, device=device)
         #model = get_instance_segmentation_model(num_classes)
         """model.to(device)
 
@@ -402,7 +509,7 @@ def temp():
         optimizer = torch.optim.SGD(params, lr=0.004, momentum=0.9, weight_decay=0.0005)
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.7)
 
-        num_epochs = 20
+        num_epochs = 15
 
         for epoch in range(num_epochs):
             train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=1)
@@ -473,18 +580,19 @@ def temp():
 
 
 if __name__ == '__main__':
-    names = os.listdir("image/00/image/")
-    for n in names:
-        if not os.path.isfile("image/00/mask/"+n[:-4]+"_mask.png"):
-            print(n)
-            os.remove("image/00/image/"+n)
-    temp()
-    names = os.listdir("image/00/image/")
-    cnt = 0
-    for n in names:
-        cnt +=1
-        if cnt == 3065:
-            print(n)
+    extract_code()
+    #names = os.listdir("image/00/image/")
+    #for n in names:
+    #    if not os.path.isfile("image/00/mask/"+n[:-4]+"_mask.png"):
+    #        print(n)
+    #        os.remove("image/00/image/"+n)
+    #temp()
+    #names = os.listdir("image/00/image/")
+    #cnt = 0
+    #for n in names:
+    #    cnt +=1
+    #    if cnt == 3065:
+    #        print(n)
     #change_name("image/crown_woman/mask/",2)
     #json_path = 'image/0203/0203mask.json'
     #img_path = 'image/0203/'
